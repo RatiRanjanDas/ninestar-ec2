a669cb5e089be6fd110a9f576aed884dbc3c7c8f
RatiRanjanDas <46194290+RatiRanjanDas@users.noreply.github.com>
2022-07-27 17:53:08 +0530

    main.tfCreated

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "Tiff2pdf-AMI-Inst-test" {
+  ami           = "rati1-ami"
+  instance_type = "t2.medium"
+  key_name               = "Tiff2pdf_key"
+  vpc_security_group_ids = ["sg-09871405e0177a0dc"]
+  subnet_id              = "subnet-08b4a46c"
+
+  tags = {
+    Name = "Tiff2pdf-AMI-Inst-testInstance"
+  }
+}
