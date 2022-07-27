28f2551d377a8d9283995e470a6f73eeeb638cc6
RatiRanjanDas <46194290+RatiRanjanDas@users.noreply.github.com>
2022-07-27 17:55:47 +0530

    modified-1

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "Tiff2pdf-AMI-Inst-test" {
   key_name               = "Tiff2pdf_key"
   vpc_security_group_ids = ["sg-09871405e0177a0dc"]
   subnet_id              = "subnet-08b4a46c"
+  vpc_id                 = "vpc-298e874d"
 
   tags = {
     Name = "Tiff2pdf-AMI-Inst-testInstance"
